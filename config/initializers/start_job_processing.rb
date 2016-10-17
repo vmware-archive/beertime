9093825dfb3ac961c894c3aff5588fe1349a0768
Robert Gravina <rgravina@pivotal.io>
2016-10-17 17:36:22 +0900

    stop logging a message every minute

--- config/initializers/start_job_processing.rb
+++ config/initializers/start_job_processing.rb
@@ -1,4 +1,5 @@
 require './config/boot'
 require './config/environment'
 
-LoggerJob.perform_in(15, "LoggerJob called from the Rails initializer.")
+# For now, stop logging this message. Add useful job here later.
+#LoggerJob.perform_in(15, "LoggerJob called from the Rails initializer.")
