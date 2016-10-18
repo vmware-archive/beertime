08a12a6ee1128c4a916baaceb289839ccaf9356d
Ichizo Umehara <iumehara@pivotal.io>
2016-10-18 17:02:32 +0900

    Adds HTTP Basic Authentication
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
+  http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USERNAME'],
+                               password: ENV['HTTP_BASIC_AUTH_PASSWORD'],
+                               except: :index
   protect_from_forgery with: :exception
 end
--- spec/requests/requests_spec.rb
+++ spec/requests/requests_spec.rb
@@ -5,7 +5,13 @@ RSpec.describe 'Requests', type: :request do
     it 'renders list of beers' do
       Beer.create(name: 'Hitachino')
 
-      get new_request_path
+      env = {
+        HTTP_AUTHORIZATION: ActionController::HttpAuthentication::Basic.encode_credentials(
+          ENV['HTTP_BASIC_AUTH_USERNAME'],
+          ENV['HTTP_BASIC_AUTH_PASSWORD']
+        )
+      }
+      get new_request_path, env: env
 
       expect(response).to have_http_status(200)
       expect(response.body).to include('<li>Hitachino</li>')
