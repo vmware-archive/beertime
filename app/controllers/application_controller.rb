73b6193755cbb1f77a70c969c3bca0cb3bdd8231
Yui Anzai <yanzai@pivotal.io>
2016-10-19 10:47:01 +0900

    Remove except param from basic auth setup
    Signed-off-by: Robert Gravina <rgravina@pivotal.io>

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   http_basic_authenticate_with name: ENV['HTTP_BASIC_AUTH_USERNAME'],
-                               password: ENV['HTTP_BASIC_AUTH_PASSWORD'],
-                               except: :index
+                               password: ENV['HTTP_BASIC_AUTH_PASSWORD']
   protect_from_forgery with: :exception
 end
